0cc7f1ff09a31c40e33cd90002284c62183fc229
itlppan <it.lppan.id@gmail.com>
2025-04-12 17:36:00 +0700

    `Update Skill component to remove Bootstrap icon and related code`

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,15 @@
 import dynamic from "next/dynamic";
 import Header from "@/components/Navbar";
 import Jumbotron from "@/components/Jumbotron";
-import Skill from "@/components/Skill";
-import Portfolio from "@/components/Portofolio";
-import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
-// Import About component dynamically with SSR disabled
+// Disable SSR for components that may use window / animations
 const About = dynamic(() => import("@/components/About"), { ssr: false });
+const Skill = dynamic(() => import("@/components/Skill"), { ssr: false });
+const Portfolio = dynamic(() => import("@/components/Portofolio"), {
+  ssr: false,
+});
+const Contact = dynamic(() => import("@/components/Contact"), { ssr: false });
 
 export default function Home() {
   return (
